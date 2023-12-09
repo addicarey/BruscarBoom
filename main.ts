01cc99c9e4474ab303d99dd997644b08c2cd92ee
ac <111868306+addicarey@users.noreply.github.com>
2023-12-09 16:07:35 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+input.onButtonPressed(Button.A, function () {
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor4, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
+    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
+    Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor4)
+    basic.showIcon(IconNames.No)
+})
